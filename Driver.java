d2f45d45cd96f5d0e442f433076613e51aa91d01
shaparang <truh277@gmail.com>
2023-05-12 00:21:01 -0400

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,7 +1,7 @@
 public class Driver {
-
+// Created this driver class to test if the code is working.
 	public static void main(String[] args) {		
-		Question[] q = QuestionManager.getQuestion();
+		Question[] q = QuestionManager.getQuestion(); // this part of the code is to retrieve the question array.
 		for(Question question : q) {
 			System.out.println(question.toText());
 		}
